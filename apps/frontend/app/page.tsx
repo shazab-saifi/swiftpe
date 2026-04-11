fcffa40881bc96f9fba3f8a9efb08f6d67ae6196
shazab-saifi <shazebsaifi92@gmail.com>
2026-04-11 15:56:11 +0530

    Fix: lint-staged

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -12,6 +12,14 @@ export default function Home() {
           height={20}
           priority
         />
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
