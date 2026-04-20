ee40a84fc8aac8bfd8617aa5844386e590efa3fa
shazab-saifi <shazebsaifi92@gmail.com>
2026-04-20 22:13:58 +0530

    Created cicd pipeline for frontend with build and push job

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  outputFileTracingRoot: path.join(__dirname, "../../"),
+  outputFileTracingIncludes: {
+    "/*": ["node_modules/sharp/**/*", "node_modules/aws-crt/dist/bin/**/*"],
+  },
 };
 
 export default nextConfig;
